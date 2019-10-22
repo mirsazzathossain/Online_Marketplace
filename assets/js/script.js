2dc88e74590006dec17ddd71a110d4ae93abbffe
mirsazzathossain <mirsazzathossain@gmail.com>
2019-10-22 12:16:57 +0600

    edited!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,11 @@ $(function(){
 });
 var large = '<div class="col-lg-3 col-sm-6"><div class="product-item"><div class="pi-pic"><div class="tag-sale">ON SALE</div><img src="assets/images/product1.jpg" alt=""><div class="pi-links"><a href="#" class="add-card"><i class="fa fa-cart-plus" aria-hidden="true"></i><span>ADD TO CART</span></a></div></div><div class="pi-text"><h6>$35,00</h6><a href="">Flamboyant Pink Top</a></div></div></div>';
 $('.product').prepend(large);
+
+$('#fileInput').change(function(){
+    $('.product-img').empty();
+    var address= $('#fileInput').val();
+    console.log(localstorage.root.getFile("product2.jpg", {create: true}));
+    
+    $('.product-img').append(`<img src="${address}" alt="">`);
+});
