d0805d4e510f531b742ee14fffb1f39343d28973
mirsazzathossain <mirsazzathossain@gmail.com>
2019-11-03 00:44:23 +0600

    single product!!

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,3 +184,28 @@ $('#addnewproduct').on('click', function(){
 		$button.parent().find('input').val(newVal);
 	});
 
+    /*------------------
+		Accordions
+	--------------------*/
+	$('.panel-link').on('click', function (e) {
+		$('.panel-link').removeClass('active');
+		var $this = $(this);
+		if (!$this.hasClass('active')) {
+			$this.addClass('active');
+		}
+		e.preventDefault();
+	});
+
+    $('.product-thumbs-track > .pt').on('click', function(){
+		$('.product-thumbs-track .pt').removeClass('active');
+		$(this).addClass('active');
+		var imgurl = $(this).data('imgbigurl');
+		var bigImg = $('.product-big-img').attr('src');
+		if(imgurl != bigImg) {
+			$('.product-big-img').attr({src: imgurl});
+			$('.zoomImg').attr({src: imgurl});
+		}
+	});
+
+
+	$('.product-pic-zoom').zoom();
